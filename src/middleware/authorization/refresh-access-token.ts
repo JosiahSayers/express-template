652c6ffe1847a5a43736881338e6dce01d5c5a43
Josiah Sayers <josiah.sayers15@gmail.com>
2021-07-22 22:43:32 -0400

    removes auth cookies when invalid refresh token is passed in

--- src/middleware/authorization/refresh-access-token.ts
+++ src/middleware/authorization/refresh-access-token.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
-import { setAuthCookies } from '../../routers/user/user-helper';
+import { InvalidRefreshToken } from '../../models/errors/authentication.errors';
+import { clearAuthCookies, setAuthCookies } from '../../routers/user/user-helper';
 import { AuthenticationService } from '../../services/authentication/authentication.service';
 
 export const refreshAccessToken = async (err: Error, req: Request, res: Response, next: NextFunction): Promise<void> => {
@@ -9,6 +10,9 @@ export const refreshAccessToken = async (err: Error, req: Request, res: Response
             setAuthCookies(authResponse, res);
             req.user = authResponse.user;
         } catch (e) {
+            if (e instanceof InvalidRefreshToken) {
+                clearAuthCookies(res);
+            }
             console.error('Unable to refresh access token', e);
         }
     }
