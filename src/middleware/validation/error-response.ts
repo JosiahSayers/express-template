3ea9a1d7bed077db2703ad765b926b9b254c02d1
Josiah Sayers <josiah.sayers15@gmail.com>
2021-07-22 22:01:47 -0400

    adds lint to ci

--- src/middleware/validation/error-response.ts
+++ src/middleware/validation/error-response.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
 
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export const ValidationErrorMapper = (err: any, req: Request, res: Response, next: NextFunction): Response => {
     if (err?.error?.isJoi) {
         return res.status(400).json({
